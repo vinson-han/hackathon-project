92c93ee4d9678bf1b169386681a5cb956bdff1f9
vinson-han <han_vinson@student.smc.edu>
2024-05-05 04:19:10 -0700

    testing

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
-app.use('/', express.static('/dist'))
+app.use('/', express.static('dist'))
 
 
 app.get('/test', (req,res) => {
