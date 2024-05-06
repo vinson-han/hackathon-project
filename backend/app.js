c7b3fcfcd17fa698927d54b422e6c552eb3ca603
vinson-han <han_vinson@student.smc.edu>
2024-05-06 01:30:37 -0700

    dist

--- backend/app.js
+++ backend/app.js
@@ -105,7 +105,7 @@ app.post('/signup', async (req, res) => {
         const existingUser = await collection.findOne({ email: email });
 
         if (existingUser) {
-            return res.json('notexist'); // Email already exists
+            return res.json('exist'); // Email already exists
         }
 
         // Hash the password
@@ -121,7 +121,7 @@ app.post('/signup', async (req, res) => {
         // Save the new user to the database
         await newUser.save();
 
-        return res.status(200).json('exist'); // User successfully created
+        res.json('notexist'); // User successfully created
     } catch (error) {
         console.error(error);
         res.status(500).json('error');
